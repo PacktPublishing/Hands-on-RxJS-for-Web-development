de343b22ebc6b2d92a8a08578a5edfd32023b90c
Poshtaruk Aleksandr <aleksandr.poshtaruk@tonicforhealth.com>
2018-07-26 00:13:15 +0300

    benefits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
+import { stepSwitcherReducer } from './step-switcher/step-switcher.reducer';
+import { StepSwitcherComponent } from './step-switcher/step-switcher.component';
+import { CounterComponent } from './counter/counter.component';
+
+let reducers = {
+    stepValue: stepSwitcherReducer
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        StepSwitcherComponent,
+        CounterComponent
+    ],
+    imports: [
+        BrowserModule,
+        StoreModule.forRoot(reducers)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/step-switcher/step-switcher.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { UPDATE_STEP } from './step-switcher.reducer';
+
+@Component({
+    selector: 'step-switcher',
+    templateUrl: './step-switcher.component.html',
+    styleUrls: ['./step-switcher.component.scss']
+})
+class StepSwitcherComponent {
+    public step = 1;
+
+    constructor(private store: Store<any>) {}
+
+    setStepValue(value) {
+        this.step = value;
+        this.store.dispatch({
+            type: UPDATE_STEP,
+            payload: value
+        })
+    }
+}
+
+export {
+    StepSwitcherComponent
+}
