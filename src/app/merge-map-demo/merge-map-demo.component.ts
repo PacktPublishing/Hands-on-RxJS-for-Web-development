471262072d68b25c45c17f11743825433b53c6da
Poshtaruk Aleksandr <aleksandr.poshtaruk@tonicforhealth.com>
2018-08-16 13:10:42 +0300

    Section4_mergeMap - unsubscribe

--- src/app/merge-map-demo/merge-map-demo.component.ts
+++ src/app/merge-map-demo/merge-map-demo.component.ts
@@ -1,36 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ajax } from 'rxjs/ajax';
 import { mergeMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
-  selector: 'app-merge-map-demo',
-  templateUrl: './merge-map-demo.component.html',
-  styleUrls: ['./merge-map-demo.component.scss']
+    selector: 'app-merge-map-demo',
+    templateUrl: './merge-map-demo.component.html',
+    styleUrls: ['./merge-map-demo.component.scss']
 })
-export class MergeMapDemoComponent implements OnInit {
-  public items: string[] = [];
-  constructor() { }
+export class MergeMapDemoComponent implements OnInit, OnDestroy {
+    public items: string[] = [];
+    private subscription: Subscription;
 
-  ngOnInit() {
+    constructor() {
+    }
 
-      this.getItems(0).subscribe(result => this.items = result)
-  }
+    ngOnInit() {
 
-  getItems(index, result = []) {
-      const baseUrl = 'http://127.0.0.1:4001/list-data?page=';
-      return ajax.get( baseUrl + index).pipe(
-          mergeMap(
-              (d) => {
-                  result = result.concat(d.response.data);
-                  if ('nextIndex' in d.response) {
-                      return this.getItems(d.response.nextIndex, result);
-                  }
-                  return of(result);
-              },
-              null, // selector function - we don't need it here.
-              1) // Maximum concurrency, 1 - to prevent race conditions
-      )
-  }
+        this.subscription = this.getItems(0).subscribe(result => this.items = result)
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
+    }
+
+    getItems(index, result = []) {
+        const baseUrl = 'http://127.0.0.1:4001/list-data?page=';
+        return ajax.get(baseUrl + index).pipe(
+            mergeMap(
+                (d) => {
+                    result = result.concat(d.response.data);
+                    if ('nextIndex' in d.response) {
+                        return this.getItems(d.response.nextIndex, result);
+                    }
+                    return of(result);
+                },
+                null, // selector function - we don't need it here.
+                1) // Maximum concurrency, 1 - to prevent race conditions
+        )
+    }
 
 }
