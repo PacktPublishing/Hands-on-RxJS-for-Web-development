7e052c094fa31115635b33039baea67d1515f1e0
Oleksandr Poshtaruk <aleksandr.poshtaruk@tonicforhealth.com>
2018-09-22 19:55:55 +0300

    Section5_Video2_repeat - refactoring

--- src/app/merge-scan-demo/merge-scan-demo.component.ts
+++ src/app/merge-scan-demo/merge-scan-demo.component.ts
@@ -1,54 +1,66 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { ajax } from 'rxjs/ajax';
-import { mergeScan } from 'rxjs/operators';
-import { fromEvent, EMPTY } from 'rxjs';
-import { Subscription } from 'rxjs/internal/Subscription';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {ajax} from 'rxjs/ajax';
+import {mergeScan, repeat} from 'rxjs/operators';
+import {fromEvent, EMPTY, defer, concat} from 'rxjs';
+import {Subscription} from 'rxjs/internal/Subscription';
+
+const baseUrl = 'http://127.0.0.1:4001/list-data?page=';
 
 @Component({
-    selector: 'app-merge-scan-demo',
-    templateUrl: './merge-scan-demo.component.html',
-    styleUrls: ['./merge-scan-demo.component.scss']
+  selector: 'app-merge-scan-demo',
+  templateUrl: './merge-scan-demo.component.html',
+  styleUrls: ['./merge-scan-demo.component.scss']
 })
 export class MergeScanDemoComponent implements OnInit {
-    public items: number[] = [1, 2, 3, 4, 5];
-    @ViewChild('moreButton') moreButton: ElementRef;
-    public disableMoreButton: boolean = false;
-    private subscription: Subscription;
+  public items: number[] = [1, 2, 3, 4, 5];
+  @ViewChild('moreButton') moreButton: ElementRef;
+  public disableMoreButton: boolean = false;
+  private subscription: Subscription;
 
-    constructor() {
-    }
+  constructor() {
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        this.subscription = this.getItems().subscribe((result: any) => {
+    this.subscription = this.getItems(2).subscribe((result: any) => {
 
-            this.items = this.items.concat(result.response.data);
+      this.items = this.items.concat(result.response.data);
 
-            if (!('nextIndex' in result.response)) {
-                this.disableMoreButton = true;
-            }
-        })
-    }
+      if (!('nextIndex' in result.response)) {
+        this.disableMoreButton = true;
+      }
+    })
+  }
 
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
-    }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
-    getItems() {
-        const baseUrl = 'http://127.0.0.1:4001/list-data?page=';
-        const fetchMoreEvents$ = fromEvent(this.moreButton.nativeElement, 'click');
+  getItems(prefetchPages: number) {
 
-        return fetchMoreEvents$.pipe(
-            mergeScan((prevAjaxResponse, next) => {
-                    if ('nextIndex' in prevAjaxResponse.response) {
-                        return ajax.get(baseUrl + prevAjaxResponse.response.nextIndex)
-                    }
-                    return EMPTY;
-                },
-                {response: {nextIndex: 1}}, // Initial acc value
-                1 // Maximum concurrency, 1 - to prevent race conditions
-            )
-        )
-    }
+    const initialData$ = this.getInitialData(prefetchPages);
+
+    const fetchMoreEvents$ = fromEvent(this.moreButton.nativeElement, 'click');
+
+    const moreItems$ = fetchMoreEvents$.pipe(
+      mergeScan((prevAjaxResponse, next) => {
+          if ('nextIndex' in prevAjaxResponse.response) {
+            return ajax.get(baseUrl + prevAjaxResponse.response.nextIndex)
+          }
+          return EMPTY;
+        },
+        {response: {nextIndex: prefetchPages}}, // Initial acc value
+        1 // Maximum concurrency, 1 - to prevent race conditions
+      )
+    );
+
+    return concat(initialData$, moreItems$);
+  }
+
+  getInitialData(prefetchPages: number) {
+    let counter=0;
+    return defer(() => ajax.get(baseUrl + counter++))
+      .pipe(repeat(prefetchPages));
+  }
 
 }
