3d7f76771e91cd0868da94942c11d81cfe24fb8f
Oleksandr Poshtaruk <aleksandr.poshtaruk@tonicforhealth.com>
2018-11-20 09:04:40 +0200

    Section7_Video5_debugging - init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,16 @@ import {StoreModule} from '@ngrx/store';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
-import {stepSwitcherReducer} from './step-switcher/step-switcher.reducer';
-import {StepSwitcherComponent} from './step-switcher/step-switcher.component';
 import {CounterComponent} from './counter/counter.component';
 
-let reducers = {
-  stepValue: stepSwitcherReducer
-};
 
 @NgModule({
   declarations: [
     AppComponent,
-    StepSwitcherComponent,
     CounterComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(reducers),
     HttpClientModule
   ],
   providers: [],
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,35 +1,38 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {map} from 'rxjs/operators';
+import {interval, Observable} from "rxjs";
+import { create } from "rxjs-spy";
+import { tag } from "rxjs-spy/operators/tag";
 
 @Component({
   selector: 'counter',
   templateUrl: './counter.component.html',
   styleUrls: ['./counter.component.scss']
 })
-class CounterComponent implements OnInit, OnDestroy {
-  public counter = 0;
-  public step = 1;
-  private clearId: any;
-
-  constructor(private store: Store<any>) {
-  }
+class CounterComponent implements OnInit {
+  public counter1$: Observable<number>;
+  public counter2$: Observable<number>;
+  public counter3$: Observable<number>;
+  public counter4$: Observable<number>;
 
   ngOnInit() {
-    this.store.select(state => state.stepValue)
-      .subscribe((stepValue) => this.step = stepValue);
+    (window as any).spy = create();
 
-    this.startCounter();
-  }
+    this.counter1$ = interval(1000).pipe(map(x => x + 1));
+    this.counter2$ = interval(1500).pipe(map(x => x * 2));
 
-  startCounter() {
-    this.clearId = setInterval(() => {
-      this.counter = this.counter + this.step;
-    }, 1000)
-  }
+    this.counter3$ = interval(1000).pipe(
+      map(x => x + 1),
+      tag('interval3')
+    );
+    this.counter4$ = interval(1500).pipe(
+      map(x => x * 2),
+      tag('interval4')
+    );
 
-  ngOnDestroy() {
-    clearInterval(this.clearId);
+    this.counter3$.subscribe();
+    this.counter4$.subscribe();
   }
 }
 
